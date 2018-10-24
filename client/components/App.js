a0ad305720f2857ad5694d88818c2ea87074ff54
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2018-10-24 03:01:55 -0400

    Added base components with base styling

--- client/components/App.js
+++ client/components/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
 
+import Menu from "./Menu";
+import Post from "./Post";
+
 import styles from '../styles/App.css';
 
 class App extends Component {
 	render() {		
-		// Remove these tags once done
 		return (
 			<div>
-				<h1 className={styles.title}>Boilerplate template</h1>
-				<div 
-					className={styles.example_button}
-					onClick={() => {this.props.onActionEvent()}}>
-					<div>{this.props.example.selected_name} has clicked!<br />Click me!</div>
-				</div>
-				<div className={styles.credits}>
-					Boilerplate revised by <a target="_blank" href="https://github.com/tiberiusuciu">Tiberius Suciu</a>
-				</div>
+				<Menu />
+				<Post />
 			</div>
 		)
 	}
---
+++ client/components/Menu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Menu extends Component {
+	render() {		
+		return (
+			<div className="menu">
+				Menu
+			</div>
+		)
+	}
+};
+
+export default Menu;
