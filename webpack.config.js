e9fa4ec523a2d765c57c7661598d67014c65f5f8
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2018-10-24 02:25:38 -0400

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./client/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  mode: "development",
+  devtool: 'inline-source-map',
+  entry: [
+		'babel-polyfill',
+		'webpack-hot-middleware/client?reload=true',
+		'./client/index.js'
+	],
+	output: {
+		path: require('path').resolve('./dist'),
+		filename: 'main.js',
+		publicPath: '/'
+	},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    htmlPlugin,
+		new webpack.HotModuleReplacementPlugin(),
+  ]
+};
